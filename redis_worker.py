aa38194fe77f50e6ef1f28e9c90d534a514b80c2
Karthik ( cr4ck_3t) <kartik997@gmail.com>
2020-10-03 19:06:06 +0530

    Add files via upload

---
+++ redis_worker.py
@@ -0,0 +1,19 @@
+import os
+
+import redis
+from rq import Connection, Queue, Worker
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "floodrelief.settings")
+redis_url = os.environ.get("REDIS_URL", "redis://localhost:6379")
+
+listen = ["high", "default", "low", "smsjob", "bulkcsvjob", "volunteergroup"]
+conn = redis.from_url(redis_url)
+
+
+if __name__ == "__main__":
+    import django
+
+    django.setup()
+    with Connection(conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
